2a2bcc65afdaab0825ff8cf4687b10bf7607df63
ningenMe <amohkuru@gmail.com>
2021-07-15 01:09:30 +0900

    [modify] font

--- src/atoms/NavLink.tsx
+++ src/atoms/NavLink.tsx
@@ -1,11 +1,13 @@
 import { Nav,Button } from 'react-bootstrap';
 import { NavlinkStyle } from 'styles/NavlinkStyle'
 import { Link } from 'interfaces/Link'
+import {KosugiMaruStyle} from 'styles/FontStyle'
 
 export const NavLink = ({link}:{link:Link}) => {
+    const style = Object.assign( {}, NavlinkStyle, KosugiMaruStyle)
     return (
         <Button variant="outline-info">
-            <Nav.Link href={link.href} style={NavlinkStyle} >{link.name}</Nav.Link>
+            <Nav.Link href={link.href} style={style} >{link.name}</Nav.Link>
         </Button>
     );
 }
