a8452915480ea2746b6af2976f4f00ebbb056f13
ningenMe <amohkuru@gmail.com>
2021-08-06 03:14:43 +0900

    [add] env

---
+++ src/organisms/HomeOrganisms.tsx
@@ -0,0 +1,62 @@
+import {Grid,Typography } from '@material-ui/core';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, {EffectFlip,Pagination,Navigation} from 'swiper/core';
+import {HomeImageCard} from 'molecules/HomeMolecules'
+import {HomeImageContent} from 'interfaces/Content'
+import {Alert} from 'react-bootstrap'
+
+SwiperCore.use([EffectFlip,Pagination,Navigation]);
+
+export const NotificationFrame = () => {
+
+  const prefix : string = "この環境は env です。";
+  const env = process.env.REACT_APP_ENV;
+
+  if(env === "stg") {
+    return (
+      <Alert variant={"danger"}>
+        {prefix.replace("env",env)}
+      </Alert>
+    );
+  }
+  if(env === "dev") {
+    return (
+      <Alert variant={"success"}>
+        {prefix.replace("env",env)}
+      </Alert>
+    );
+  }
+  return <></>      
+}
+
+export const ImageSlideFrame = () => {
+  const imageContents : ReadonlyArray<HomeImageContent> = [
+    {title:"slide1",src:"/slide/slide1.jpeg"},
+    {title:"slide2",src:"/slide/slide2.jpeg"},
+    {title:"slide3",src:"/slide/slide3.jpeg"},
+    {title:"slide4",src:"/slide/slide4.jpeg"},
+  ]
+
+  return (
+    <Swiper
+    effect={'flip'}
+    grabCursor={true}
+    spaceBetween={300}
+    slidesPerView={1}
+    pagination={{"clickable": true}}
+    navigation={true}
+    style={{
+      padding:"50px"
+    }}
+  >
+    {imageContents.map((content:HomeImageContent) => {
+      return (
+        <SwiperSlide>
+          <HomeImageCard content={content} />
+        </SwiperSlide>  
+      );
+    })}
+  </Swiper>
+  );
+
+}
