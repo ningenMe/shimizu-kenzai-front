b9d2ba1934742433a217d44438aa982065c19f31
ningenMe <amohkuru@gmail.com>
2021-08-06 03:50:07 +0900

    [fix]

--- src/constants/WorkConstants.ts
+++ src/constants/WorkConstants.ts
@@ -1,29 +1,30 @@
 import {WorkImageContent,WorkContent} from 'interfaces/Content'
+const prefix = process.env.REACT_APP_PATH_PREFIX;
 
 export const itemContents : ReadonlyArray<WorkImageContent> = [
     {
         title:"鉄鋼二次製品",
         body:"丸釘、針金、ナマシ線、一般鋼材、カラー平波板、溶接金網", 
         imageTitle:"iron",  
-        src:"/iron.jpeg"
+        src:prefix + "/iron.jpeg"
     },
     {
         title:"建材製品",
         body:"メタルラス、リブラス、クエント、ルーフィング、グレーチング、塩ビ波板ドレン、雨樋、面木、エアーネイラー、特殊釘、養生材、建設資材", 
         imageTitle:"material",  
-        src:"/material.jpeg"
+        src:prefix + "/material.jpeg"
     },
     {
         title:"建材金物",
         body:"各種金物全般", 
         imageTitle:"hardware",  
-        src:"/hardware.jpeg"
+        src:prefix + "/hardware.jpeg"
     },
     {
         title:"エクステリア",
         body:"各種フェンス、門扉、物置、ガレージ、自転車置場、ガードレール、防護柵", 
         imageTitle:"exterior",  
-        src:"/exterior.jpeg"
+        src:prefix + "/exterior.jpeg"
     },
 ]
 
@@ -43,12 +44,12 @@ export const constructContents : ReadonlyArray<WorkImageContent> = [
         title:"エクステリア",
         body:"ネットフェンス、メッシュフェンス、エキスパンドフェンス、目隠しフェンス、門扉、伸縮門扉、カーポート、自転車置場、バルコニー、テラス、物置、ガードレール、組み立てハウス、手摺", 
         imageTitle:"exterior",  
-        src:"/exterior.jpeg"
+        src:prefix + "/exterior.jpeg"
     },
     {
         title:"防球ネット",
         body:"PCホール", 
         imageTitle:"net",  
-        src:"/net.jpg"
+        src:prefix + "/net.jpg"
     },
 ]
